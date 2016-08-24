c40bae2e9189b9a607fd627f1df80f525b0bbf4c
Derek Hewitt <derekmhewitt@gmail.com>
2016-08-23 20:12:21 -0700

    added templates, working through the lecture notes

--- learning_journal/views.py
+++ learning_journal/views.py
@@ -1,6 +1,18 @@
-from pyramid.view import view_config
+# from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'learning_journal'}
+# @view_config(route_name='home', renderer='templates/mytemplate.pt')
+# def my_view(request):
+#     return {'project': 'learning_journal'}
+
+
+def list_view():
+
+
+def detail_view():
+
+
+def create_view():
+
+
+def update_view():
