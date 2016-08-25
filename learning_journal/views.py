f534a41a8bb2711b9f6c802145db34a6aa5c2253
Derek Hewitt <derekmhewitt@gmail.com>
2016-08-24 20:04:05 -0700

    views reconfigured, basic templates up and running.  Big thanks to Cris for sitting with me for a few minutes and explaining where I messed up.

--- learning_journal/views.py
+++ learning_journal/views.py
@@ -1,28 +1,23 @@
 from pyramid.view import view_config
-import os
-
-HERE = os.path.dirname(__file__)
 
 
-@view_config(route_name='home', renderer='jinja2')
+@view_config(route_name='home', renderer='templates/index.jinja2')
 def home_view(request):
-    imported_text = open(os.path.join(HERE, 'templates/index.html')).read()
-    return imported_text
+    return {}
 
 
-@view_config(route_name='detail', renderer='jinja2')
+@view_config(route_name='detail', renderer='templates/detail.jinja2')
 def detail_view(request):
-    imported_text = open(os.path.join(HERE, 'templates/detail.html')).read()
-    return imported_text
+    return {"title": "a title here",
+            "date": "24th Foo 2016",
+            "content": "<p>a little <b>bit</b> of content</p>"}
 
 
-@view_config(route_name='form', renderer='jinja2')
+@view_config(route_name='form', renderer='templates/form.jinja2')
 def form_view(request):
-    imported_text = open(os.path.join(HERE, 'templates/form.html')).read()
-    return imported_text
+    return {}
 
 
-@view_config(route_name='edit', renderer='jinja2')
+@view_config(route_name='edit', renderer='templates/edit.jinja2')
 def edit_view(request):
-    imported_text = open(os.path.join(HERE, 'templates/edit.html')).read()
-    return imported_text
+    return {}
