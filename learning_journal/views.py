2aa1e80ddad8a86e5742f44fd9d36ec7b64b60e6
Derek Hewitt <derekmhewitt@gmail.com>
2016-08-24 19:09:47 -0700

    reworked views.py, renamed html files to jinja2

--- learning_journal/views.py
+++ learning_journal/views.py
@@ -1,31 +1,28 @@
-from pyramid.response import Response
+from pyramid.view import view_config
 import os
 
 HERE = os.path.dirname(__file__)
 
 
-def includeme(config):
-    config.add_view(home_view, route_name="home")
-    config.add_view(detail_view, route_name="detail")
-    config.add_view(form_view, route_name="form")
-    config.add_view(edit_view, route_name="edit")
-
-
+@view_config(route_name='home', renderer='jinja2')
 def home_view(request):
     imported_text = open(os.path.join(HERE, 'templates/index.html')).read()
-    return Response(imported_text)
+    return imported_text
 
 
+@view_config(route_name='detail', renderer='jinja2')
 def detail_view(request):
     imported_text = open(os.path.join(HERE, 'templates/detail.html')).read()
-    return Response(imported_text)
+    return imported_text
 
 
+@view_config(route_name='form', renderer='jinja2')
 def form_view(request):
     imported_text = open(os.path.join(HERE, 'templates/form.html')).read()
-    return Response(imported_text)
+    return imported_text
 
 
+@view_config(route_name='edit', renderer='jinja2')
 def edit_view(request):
     imported_text = open(os.path.join(HERE, 'templates/edit.html')).read()
-    return Response(imported_text)
+    return imported_text
