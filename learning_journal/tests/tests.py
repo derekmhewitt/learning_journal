56092aeb22aa27f5c105e192441abc83486ecf2c
Derek Hewitt <derekmhewitt@gmail.com>
2016-09-03 15:30:01 -0700

    working on testing

--- learning_journal/tests/tests.py
+++ learning_journal/tests/tests.py
@@ -1,65 +1,55 @@
-import unittest
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+"""Imported baseline testing setup from:
+https://codefellows.github.io/sea-python-401d4/lectures/pyramid_day3.html
+per instructions there.
+"""
+import pytest
 import transaction
+import datetime
 
 from pyramid import testing
 
+from ..models import (
+    Entry,
+    get_engine,
+    get_session_factory,
+    get_tm_session,
+)
+from ..models.meta import Base
 
-def dummy_request(dbsession):
-    return testing.DummyRequest(dbsession=dbsession)
 
+@pytest.fixture(scope="session")
+def sqlengine(request):
+    config = testing.setUp(settings={
+        'sqlalchemy.url': 'sqlite:///:memory:'
+    })
+    config.include("..models")
+    settings = config.get_settings()
+    engine = get_engine(settings)
+    Base.metadata.create_all(engine)
 
-class BaseTest(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp(settings={
-            'sqlalchemy.url': 'sqlite:///:memory:'
-        })
-        self.config.include('.models')
-        settings = self.config.get_settings()
-
-        from .models import (
-            get_engine,
-            get_session_factory,
-            get_tm_session,
-            )
-
-        self.engine = get_engine(settings)
-        session_factory = get_session_factory(self.engine)
-
-        self.session = get_tm_session(session_factory, transaction.manager)
-
-    def init_database(self):
-        from .models.meta import Base
-        Base.metadata.create_all(self.engine)
-
-    def tearDown(self):
-        from .models.meta import Base
-
+    def teardown():
         testing.tearDown()
         transaction.abort()
-        Base.metadata.drop_all(self.engine)
+        Base.metadata.drop_all(engine)
+
+    request.addfinalizer(teardown)
+    return engine
 
 
-class TestMyViewSuccessCondition(BaseTest):
+@pytest.fixture(scope="function")
+def new_session(sqlengine, request):
+    session_factory = get_session_factory(sqlengine)
+    session = get_tm_session(session_factory, transaction.manager)
 
-    def setUp(self):
-        super(TestMyViewSuccessCondition, self).setUp()
-        self.init_database()
+    def teardown():
+        transaction.abort()
 
-        from .models import MyModel
-
-        model = MyModel(name='one', value=55)
-        self.session.add(model)
-
-    def test_passing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'learning_journal')
+    request.addfinalizer(teardown)
+    return session
 
 
-class TestMyViewFailureCondition(BaseTest):
-
-    def test_failing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info.status_int, 500)
+def test_entry_model_insert(new_session):
+    assert len(new_session.query(Entry).all()) == 0
+    model = Entry(title="", creation_date=datetime.datetime.now(), body="")
