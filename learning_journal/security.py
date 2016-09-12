078c962a854c94253029f378affdd90870235d5c
Derek Hewitt <derekmhewitt@gmail.com>
2016-09-11 23:30:45 -0700

    lots of security work done

--- learning_journal/security.py
+++ learning_journal/security.py
@@ -1,7 +1,46 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import os
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.security import Allow, Everyone, Authenticated
+from passlib.apps import custom_app_context as pwd_context
 """Security for my Pyramid configuration."""
 
 
 def includeme(config):
-    pass
+    """Defines our apps security policy."""
+    auth_secret = os.environ.get('AUTH_SECRET', 'itsreallyreallyforrealsiesaseekrit')
+    authn_policy = AuthTktAuthenticationPolicy(
+        secret=auth_secret,
+        hashalg='sha512'
+    )
+    config.set_authentication_policy(authn_policy)
+    authz_policy = ACLAuthorizationPolicy()
+    config.set_authorization_policy(authz_policy)
+    config.set_default_permission('view')
+    config.set_root_factory(RootACL)
+
+
+class RootACL(object):
+    """Defines the ACL for our app."""
+    def __init__(self, request):
+        self.request = request
+
+    __acl__ = [
+        (Allow, Everyone, 'view'),
+        (Allow, Authenticated, 'secret'),
+    ]
+
+
+def check_credentials(username, password):
+    stored_username = os.environ.get('AUTH_USERNAME', '')
+    stored_password = os.environ.get('AUTH_PASSWORD', '')
+    is_authenticated = False
+    if stored_username and stored_password:
+        if username == stored_username:
+            try:
+                is_authenticated = pwd_context.verify(password, stored_password)
+            except ValueError:
+                pass
+    return is_authenticated
