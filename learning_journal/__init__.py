f55504458b3e334b97c6cce899a29b297a9c18b3
Derek Hewitt <derekmhewitt@gmail.com>
2016-09-11 22:22:34 -0700

    moved 1 critical line of code up a smidge

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -8,8 +8,8 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
 
     Huzzah!  Derek likes Tacos."""
-    config = Configurator(settings=settings)
     settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL', '')
+    config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
     config.include('.routes')
